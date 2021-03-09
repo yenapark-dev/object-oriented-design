fe53884e6b311a002ae1da0dc7907ccda41d9c5f
yenapark-dev <yn374912@gmail.com>
2021-03-09 16:54:16 +1000

    first commit

--- FileComparison/Program.cs
+++ FileComparison/Program.cs
@@ -11,13 +11,3 @@ namespace FileComparison
     }
 }
 
-//Create a program named FileComparison that compares two files.
-//First, use a text editor such as Notepad to save your favourite
-//movie quote. Next, copy the file contents, and paste them into a
-//word-processing program such as Word. Then, write the
-//file-comparison application that displays the sizes of the
-//two files as well as the ratio of their sizes to each other.
-//To discover a file’s size, you can create a System.IO.FileInfo
-//object using statements such as the following, where FILE_NAME
-//is a string that contains the name of the file, and size has been
-//declared as an integer:
